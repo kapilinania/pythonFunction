ab21e2469376174375670e2fb0716add812ac67e
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-15 20:02:35 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# import random
+# for i in range(1, 10):
+#     print(random.randint(0, 20))
+
+# we are doing with function
+def addition(a, b):
+    result = a + b
+    return result
+
+
+print(addition(2, 4))
+
+
+# multiply function
+def multi(a, b):
+    result = a * b
+    return result
+
+
+print(multi(5, 10))
+
+print(addition(multi(5, 10), 2))
+
+print("hello world")
+print("bye world")
+print("check world")
