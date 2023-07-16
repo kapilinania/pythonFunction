0ca957bcbf2f8c63afb7beda76492422fa26731f
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-16 14:37:01 +0530

    Use pandas library

---
+++ packagedemo.py
@@ -0,0 +1,8 @@
+import pandas as kapil
+# use as keyword we can change name here given example
+# import pandas
+# df = pandas.read_excel("testpandas.xlsx")
+# print(df)
+
+df = kapil.read_excel("testpandas.xlsx")
+print(df)
