12787c2efcd7f3a79692167b8ad3bfe433bf4a25
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-16 14:08:48 +0530

    comment basic

---
+++ basic.py
@@ -0,0 +1,65 @@
+# first program
+print("task")
+print("Hello world")
+# comments in python
+# ctrl + forward slash (#)
+'''
+print(5+10)
+print(10*2)   this is multiline comment
+print("welcome python ")
+'''
+# variable  in  python
+# to store data we are use varable
+
+a = 10
+print(a)
+a = 6.5
+print(a)
+c = True  # boolean type
+print(c)
+a = "kapil"
+print(a)
+a = 'jodhpur'
+print(a)
+e = None
+print(e)
+# typecasting
+a = 20
+print(a + 5)
+b = "30"
+print(int(b) + 5)  # here i am covert string to int
+
+# Here I am doing operator
+
+num1 = 50
+num2 = 10
+# Arithmetic operator
+print("num1 + num2 is = ", num1 + num2)
+print("num1 - num2 is = ", num1 - num2)
+print("num1 * num2 is = ", num1 * num2)
+print("num1 / num2 is = ", num1 / num2)
+print("num1 // num2 is = ", num1 // num2)  # point k bd wali nhi show hogi
+print("num1 ** num2 is = ", num1 ** num2)
+print("num1 % num2 is = ", num1 % num2)
+
+# Assignment operator
+a = 100
+a += 10.23
+a -= 11
+print(a)
+
+# comparison operator
+# this is compare two value
+x = 100
+y = 10
+z = 100
+print(x > y)
+print(x < y)
+print(x != y)
+print(x == z)
+
+# logical operator
+print(x == z and x < y)
+print(x > z or x < y)
+print(x == z and x < y)
+print(not (False))
