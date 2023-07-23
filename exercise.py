d542a23005004fbb56acc881640fad4499ad9a0c
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-23 17:53:40 +0530

    exercise

--- exercise.py
+++ exercise.py
@@ -36,15 +36,64 @@ def feactorial_num(a):
 
 
 res = feactorial_num(5)
-print(f"factorial is= ",res)
+print(f"factorial is= ", res)
+
 
 # Function to find the largest element in a list
 
 def find_largest_number(lst):
     largest = lst[0]
-    for num in  lst:
+    for num in lst:
         if num > largest:
             largest = num
     return largest
-numbers = [10,0,4,50,99,45,12]
+
+
+numbers = [10, 0, 4, 50, 99, 45, 12]
 print(f"the largest number is {find_largest_number(numbers)}")
+
+""" here  we are learn some variable example"""
+
+
+# Write a Python program that swaps the values of two variables a and b without using a temporary variable.
+
+def swap_data(a, b):
+    print(f"Before swap a = {a} and b = {b}")
+    a, b = b, a
+    print(f"After swap a = {a}, and b = {b}")
+
+
+swap_data(10, 20)
+
+
+# Write a Python function that takes a list of numbers as input and returns the sum and average of the numbers as a
+# tuple.
+def task(number):
+    data = sum(numbers)
+    return data
+
+
+numbers = [10, 20, 30, 40, 50]
+data = sum(numbers) / len(numbers)
+print(f"sum of list is = ", data)
+
+# Write a Python program that prompts the user to enter their name and age.
+# Then, print a message saying, "Hello, {name}! You are {age} years old."
+
+a = input("enter your age:")
+b = input("Enter Your name:")
+print("Your age is", a)
+print("Your name is", b)
+
+
+# is here
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+celsius_to_fahrenheit(50)
+fahrenheit_to_celsius(100)
